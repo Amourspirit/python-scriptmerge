01040c6d5f7e64dd726d5cb5be9741d1f26474fb
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-09-24 15:53:47 -0400

    drop older support. Now requires python >= 3.7

--- stickytape/main.py
+++ stickytape/main.py
@@ -3,6 +3,7 @@ import sys
 
 import stickytape
 
+
 def main():
     args = _parse_args()
     output_file = _open_output(args)
@@ -11,15 +12,18 @@ def main():
         add_python_modules=args.add_python_module,
         add_python_paths=args.add_python_path,
         python_binary=args.python_binary,
-        copy_shebang=args.copy_shebang
+        copy_shebang=args.copy_shebang,
+        exclude_python_modules=args.exclude_python_module,
     )
     output_file.write(output)
+
 
 def _open_output(args):
     if args.output_file is None:
         return sys.stdout
     else:
         return open(args.output_file, "w")
+
 
 def _parse_args():
     parser = argparse.ArgumentParser()
@@ -32,6 +36,6 @@ def _parse_args():
     parser.add_argument("--copy-shebang", action="store_true")
     return parser.parse_args()
 
+
 if __name__ == "__main__":
     main()
-
